0a8ba33414095eae079fc71a875da0fa148901e0
ruby20701 <kathryndobson.fr@gmail.com>
2025-09-30 09:14:57 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [mdx()],
   site: 'https://asianhornet.com',
+  integrations: [
+    mdx(),
+    sitemap()   // ✅ added sitemap integration
+  ],
   markdown: {
     rehypePlugins: [
       ["rehype-external-links", { target: "_blank", rel: ["noopener", "noreferrer"] }]
     ]
   }
 });
+
